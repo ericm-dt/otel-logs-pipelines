7ff1b85951a96fc5b1f962e87a8b183afc9ed5c2
Eric Mittelhammer <eric.mittelhammer@dynatrace.com>
2026-03-18 20:03:19 -0400

    added backend, made vpc configurable, added otel collector config vars

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -13,7 +13,53 @@ resource "helm_release" "otel_demo" {
   version    = var.otel_demo_chart_version
   namespace  = kubernetes_namespace.otel_demo.metadata[0].name
 
-  # Wait for all pods to be ready before marking the release as successful.
+  # Merge custom collector configuration with defaults
+  values = [
+    yamlencode({
+      opentelemetryCollector = merge(
+        # Default collector configuration (applied when custom config not specified)
+        {
+          config = {
+            receivers = {
+              otlp = {
+                protocols = {
+                  grpc = {}
+                  http = {}
+                }
+              }
+            }
+            processors = {
+              batch = {}
+            }
+            exporters = {}
+            service = {
+              pipelines = {
+                traces = {
+                  receivers = ["otlp"]
+                  exporters = []
+                }
+                metrics = {
+                  receivers = ["otlp"]
+                  exporters = []
+                }
+                logs = {
+                  receivers = ["otlp"]
+                  exporters = []
+                }
+              }
+            }
+          }
+          env = {}
+        },
+        # Custom overrides
+        {
+          config = var.otel_collector_config
+          env = var.otel_collector_env
+        }
+      )
+    })
+  ]
+
   wait    = true
   timeout = 600
 
